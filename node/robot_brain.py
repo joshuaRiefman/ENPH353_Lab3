ee2b16a43bcc0cc4a38bbbb9ca56cf20b1921b17
joshua Riefman <joshuariefman@gmail.com>
2024-09-18 07:12:53 +0000

    Better line following

--- node/robot_brain.py
+++ node/robot_brain.py
@@ -16,6 +16,14 @@ PUBLISH_TOPIC_NAME: str = '/cmd_vel'
 IMAGE_PUB_NAME: str = '/robot/vision'
 UNKNOWN_POSITION: int = -1
 
+ROBOT_SPEED_LINEAR = 0.66
+ROBOT_SPEED_ANGULAR = 5
+
+
+# pts1 = np.array([[50, 50], [390, 50], [50, 390], [390, 390]], dtype=np.float32)
+# pts2 = np.array([[0, 0], [800, 0], [0, 800], [800, 800]], dtype=np.float32)
+# M = cv.getPerspectiveTransform(pts1, pts2)
+
 
 def get_new_position(new_position, prev_position, dt) -> float:
     velocity = abs(new_position - prev_position)
@@ -77,34 +85,44 @@ class Brain:
     def _update(self, data: np.ndarray):
         try:
             cv_image: np.ndarray = self._bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+            # cv_image = cv.warpPerspective(raw_cv_image, M, [800, 800])
         except Exception as e:
             rospy.loginfo(e)
             return
         
-        
+        found_line = False
+        height: int = cv_image.shape[1]
+
         try:
             center, row = detect_line_center(cv_image)
+
+            found_line = True
+
         except NoVisibleRoadException:
             # This will cause the robot to just rotate
-            normalized_center = 0.5    
+            center = height / 2
+            row = 0 # Not functional, just for cosmetics
 
-        rospy.loginfo(f"Center is: {center}")
-        rospy.loginfo(f"Row is {row}")
-
-        height: int = cv_image.shape[1]
-        normalized_center = float(center) / float(height)
-
+        # Compute normalized displacement of line position from center
+        normalized_center = (float(center) / float(height)) - 0.5
+        
+        # Now, scale the value to counteract edge widening bias
+        adjusted_normalized_center = np.sign(normalized_center) * np.sqrt(np.abs(normalized_center))
+        
         try:   
-            vision = self._bridge.cv2_to_imgmsg(cv.circle(cv_image, (int(center), int(height) - int(row)), 10, (0, 0, 255), thickness=-1), encoding="bgr8")
+            vision = self._bridge.cv2_to_imgmsg(cv.circle(cv_image, (int(center), int(height) - int(row) - 40), 20, (0, 0, 255), thickness=-1), encoding="bgr8")
             self._image_pub.publish(vision)
         except Exception as e:
             rospy.loginfo(e)
 
-        if normalized_center > 0.5:
-            self._move.angular.z = -5.0
-        else:
-            self._move.angular.z = 5.0
-        
+        self._move.angular.z = adjusted_normalized_center * -ROBOT_SPEED_ANGULAR
+
+        # Only move forwards when we are seeing the line!
+        if found_line:
+            # Reduce speed when not centered nicely
+            reduction_factor = max(0, 1 - (3 * np.abs(adjusted_normalized_center)))
+            self._move.linear.x = ROBOT_SPEED_LINEAR * reduction_factor
+
 
     def move(self):
         self._pub.publish(self._move)
